9e090142495e27027a40f4072c595e13e9aa7e10
Sander Kondratjev <sanderko@hotmail.com>
2022-08-14 16:32:22 +0300

    Improve controller and add main sectors to db

--- src/main/java/com/task/skillshowing/SkillShowingApplication.java
+++ src/main/java/com/task/skillshowing/SkillShowingApplication.java
@@ -1,13 +1,34 @@
 package com.task.skillshowing;
 
+import com.task.skillshowing.model.Sector;
+import com.task.skillshowing.repository.SectorInvolvementRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SkillShowingApplication {
+public class SkillShowingApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SkillShowingApplication.class, args);
 	}
 
+	@Autowired
+	private SectorInvolvementRepository sectorInvolvementRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Sector sector1 = new Sector();
+		sector1.setName("Manufacturing");
+		sectorInvolvementRepository.save(sector1);
+
+		Sector sector2 = new Sector();
+		sector2.setName("Other");
+		sectorInvolvementRepository.save(sector2);
+
+		Sector sector3 = new Sector();
+		sector3.setName("Service");
+		sectorInvolvementRepository.save(sector3);
+	}
 }
--- src/main/java/com/task/skillshowing/controller/SectorInvolvementController.java
+++ src/main/java/com/task/skillshowing/controller/SectorInvolvementController.java
@@ -1,20 +1,28 @@
 package com.task.skillshowing.controller;
 
-import com.task.skillshowing.service.SectorInvolvementService;
+import com.task.skillshowing.model.Sector;
+import com.task.skillshowing.repository.SectorInvolvementRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin
+import java.util.List;
+
 @RestController
+@RequestMapping("/api/v1/sectors")
 public class SectorInvolvementController {
 
     @Autowired
-    private SectorInvolvementService sectorInvolvementService;
+    private SectorInvolvementRepository sectorInvolvementRepository;
 
     @GetMapping("/")
     public String info() {
         return "Test that back-end works";
+    }
+
+    @GetMapping
+    public List<Sector> getAllSectors() {
+        return sectorInvolvementRepository.findAll();
     }
 }
