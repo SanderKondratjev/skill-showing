d213b7d8dce92ed8a1d179433f2f9ba492cff4cd
Sander Kondratjev <sanderko@hotmail.com>
2022-08-14 22:55:06 +0300

    Get Posting working from browser side

--- skill-frontend/src/components/ListSectorComponent.js
+++ skill-frontend/src/components/ListSectorComponent.js
@@ -8,34 +8,67 @@ const ListSectorComponent = () => {
     useEffect(() => {
 
         SectorService.getAllSectors().then((response) => {
-          setSectors(response.data)
-          console.log(response.data)
-        }).catch(error =>{
+            setSectors(response.data)
+            console.log(response.data)
+        }).catch(error => {
             console.log(error);
         })
 
     }, []);
 
+    const [name, setName] = useState('')
+    const saveSector = (e) => {
+        e.preventDefault();
+
+        const sector = {name}
+
+        console.log(sector);
+    }
+
 
     return (
+
         <div className="container">
-            <h2 className="text-center"> List Sectors </h2>
-            <table className="table table-bordered table-striped">
-                <thead>
-                <th> Sectors</th>
-                <th> Actions</th>
-                </thead>
-                <tbody>
-                {
-                    sectors.map(
-                        sector =>
-                            <tr key={sector.id}>
-                                <td> {sector.name} </td>
-                            </tr>
-                    )
-                }
-                </tbody>
-            </table>
+            <br/><br/>
+            <div className="card">
+                <h5 className="text-center"> Please enter your name and pick the Sectors you are currently involved
+                    in.</h5>
+                <div className="card-body">
+                    <form>
+                        <div className="form-group mb-2">
+                            <label className="form-label"> Name :</label>
+                            <input
+                                type="text"
+                                placeholder="Enter Sector name"
+                                className="form-control"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                            >
+                            </input>
+                            <h2 className="text-center"> List of Sectors </h2>
+                            <table className="table table-bordered table-striped">
+                                <thead>
+                                <th> Sectors</th>
+                                <th> Actions</th>
+                                </thead>
+                                <tbody>
+                                {
+                                    sectors.map(
+                                        sector =>
+                                            <tr key={sector.id}>
+                                                <td> {sector.name} </td>
+                                            </tr>
+                                    )
+                                }
+                                </tbody>
+                            </table>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            <button className="btn btn-success" onClick={(e) => saveSector(e)}>Submit</button>
+
         </div>
     )
 }
