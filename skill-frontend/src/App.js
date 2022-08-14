cfad7348dbbb4ebc18f5ebc95c72919ded6b29f0
Sander Kondratjev <sanderko@hotmail.com>
2022-08-14 20:45:01 +0300

    Add header and footer

--- skill-frontend/src/App.js
+++ skill-frontend/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import ListSectorComponent from "./components/ListSectorComponent";
+import HeaderComponent from "./components/HeaderComponent";
+import FooterComponent from "./components/FooterComponent";
 
 function App() {
     return (
         <div>
+            <HeaderComponent/>
             <ListSectorComponent/>
+            <FooterComponent/>
         </div>
     );
 }
---
+++ skill-frontend/src/components/HeaderComponent.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const HeaderComponent = () => {
+    return (
+        <div>
+            <header>
+                <nav className = "navbar navbar-expand-md navbar-dark bg-dark">
+                    <div>
+                        <a href = "https://www.investopedia.com/terms/s/sector.asp" className = "navbar-brand">
+                            Sector Picking Application
+                        </a>
+                    </div>
+
+                </nav>
+            </header>
+        </div>
+    )
+}
+
+export default HeaderComponent
