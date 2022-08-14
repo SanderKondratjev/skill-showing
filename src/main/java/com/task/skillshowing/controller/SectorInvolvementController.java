39101dc25d9752c90a4a0c8d4118121f785f3f8e
Sander Kondratjev <sanderko@hotmail.com>
2022-08-14 22:13:05 +0300

    Test out Posting

--- src/main/java/com/task/skillshowing/controller/SectorInvolvementController.java
+++ src/main/java/com/task/skillshowing/controller/SectorInvolvementController.java
@@ -3,10 +3,7 @@ package com.task.skillshowing.controller;
 import com.task.skillshowing.model.Sector;
 import com.task.skillshowing.repository.SectorInvolvementRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,13 +15,15 @@ public class SectorInvolvementController {
     @Autowired
     private SectorInvolvementRepository sectorInvolvementRepository;
 
-    @GetMapping("/")
-    public String info() {
-        return "Test that back-end works";
-    }
-
     @GetMapping
     public List<Sector> getAllSectors() {
         return sectorInvolvementRepository.findAll();
+    }
+
+    // TODO change it later to filler name, currently just check that everything works
+    // TODO for sectors u dont need to use "/sectors after postmapping as it is already in line 12
+    @PostMapping
+    public Sector createSector(@RequestBody Sector sector) {
+        return sectorInvolvementRepository.save(sector);
     }
 }
