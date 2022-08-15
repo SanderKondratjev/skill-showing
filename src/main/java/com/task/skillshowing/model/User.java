4f0f22b8b315a5de18d73ba072b29d6e29a312cf
Sander Kondratjev <sanderko@hotmail.com>
2022-08-15 17:55:31 +0300

    Get liquibase working

---
+++ src/main/java/com/task/skillshowing/model/User.java
@@ -0,0 +1,27 @@
+package com.task.skillshowing.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "accepted_terms")
+    private Boolean accepted_terms;
+}
