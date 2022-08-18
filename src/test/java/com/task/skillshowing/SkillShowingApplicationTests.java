c8dae81f15e5132fb764b5fc8d2c4f1cc35696b3
Sander Kondratjev <sanderko@hotmail.com>
2022-08-18 17:27:52 +0300

    Add a couple of tests

--- src/test/java/com/task/skillshowing/SkillShowingApplicationTests.java
+++ src/test/java/com/task/skillshowing/SkillShowingApplicationTests.java
@@ -1,13 +1,33 @@
 package com.task.skillshowing;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class SkillShowingApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Test
+    public void getStatusOkWhenGettingAllSectors() throws Exception {
+        this.mockMvc
+                .perform(get("/api/v1/sectors/"))
+                .andDo(print()).andExpect(status().isOk());
+    }
+
+    @Test
+    void getStatusOkWhenGettingOneSector() throws Exception {
+        this.mockMvc
+                .perform(get("/api/v1/sectors/{id}", "1"))
+                .andDo(print()).andExpect(status().isOk());
+    }
 }
