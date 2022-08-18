62f55e9a5c22412d215b753160314ccc291d4b52
Sander Kondratjev <sanderko@hotmail.com>
2022-08-18 17:30:28 +0300

    Add unit test also

--- src/test/java/com/task/skillshowing/SkillShowingApplicationTests.java
+++ src/test/java/com/task/skillshowing/SkillShowingApplicationTests.java
@@ -1,11 +1,13 @@
 package com.task.skillshowing;
 
+import com.task.skillshowing.model.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -29,5 +31,13 @@ class SkillShowingApplicationTests {
         this.mockMvc
                 .perform(get("/api/v1/sectors/{id}", "1"))
                 .andDo(print()).andExpect(status().isOk());
+    }
+
+    @Test
+    public void testEmployeeName() {
+        User user = new User();
+        user.setName("User Name");
+        String userName = user.getName();
+        assertEquals("User Name", userName);
     }
 }
